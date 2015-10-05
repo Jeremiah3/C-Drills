9ec5362185c2000377220087335da6498b8f16f4
Jeremiah3 <jwalkin_it24@icloud.com>
2015-10-05 02:06:44 -0700

    Reading data from a text file

---
+++ ReadFromTextFile/ReadFromTextFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadFromTextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader txtReader = new StreamReader("Info.txt");
+            string line = "";
+
+            while (line != null)
+            {
+                line = txtReader.ReadLine();
+                if (line != null)
+                    Console.WriteLine(line);
+            }
+            txtReader.Close();
+            Console.ReadLine();
+        }
+    }
+}
