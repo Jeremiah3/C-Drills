187cce41cd23d47e79c95926345425538f8597f4
Jeremiah3 <jwalkin_it24@icloud.com>
2015-10-05 21:14:09 -0700

    Just playing around with Dates and time

---
+++ DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This code below retrieves the local date and time.
+            //There are lots of methods for retrieving the date and time in different formats.
+
+            DateTime myValue = DateTime.Now;
+            Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
